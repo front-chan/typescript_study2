6f0131e62c225d100b825d3e4729c38df70459f9
front-chan <chan.jor.zz@gmail.com>
2023-03-10 02:45:21 +0900

    TypeScript Type vs JavaScript Type

--- start_TS/app.ts
+++ start_TS/app.ts
@@ -1,1 +1,21 @@
-console.log("Your code goes here...");
+// 매개변수 이름 다음에 타입 이름 작성
+// function add(n1, n2) {
+function add(n1: number, n2: number) {
+    // console.log(typeof number1);
+
+    // TypeScript 사용을 피하고 있는 코드
+    // if (typeof n1 === 'number' && typeof n2 === 'number') {
+    //     return n1 + n2;
+    // }
+    // if (typeof n1 !== "number" || typeof n2 !== "number") {
+    //     throw new Error("Incorrect input!");
+    // }
+    return n1 + n2;
+}
+
+const number1 = 5;
+// const number1 = "5"; // 52.8 출력
+const number2 = 2.8;
+
+const result = add(number1, number2);
+console.log(result);
