f06ee609bf2fb683c4a74afe05e66697cdcf0426
front-chan <chan.jor.zz@gmail.com>
2023-03-10 02:54:24 +0900

    basic types

--- start_TS/app.js
+++ start_TS/app.js
@@ -1,18 +1,18 @@
 // 매개변수 이름 다음에 타입 이름 작성
-// function add(n1, n2) {
-function add(n1, n2) {
-    // console.log(typeof number1);
-    // TypeScript 사용을 피하고 있는 코드
-    // if (typeof n1 === 'number' && typeof n2 === 'number') {
-    //     return n1 + n2;
-    // }
+function add(n1, n2, showResult, phrase) {
     // if (typeof n1 !== "number" || typeof n2 !== "number") {
     //     throw new Error("Incorrect input!");
     // }
-    return n1 + n2;
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return n1 + n2;
+    }
 }
 var number1 = 5;
-// const number1 = "5"; // 52.8 출력
 var number2 = 2.8;
-var result = add(number1, number2);
-console.log(result);
+var printResult = true;
+var resultPhrase = "Result is: ";
+add(number1, number2, printResult, resultPhrase);
--- start_TS/app.ts
+++ start_TS/app.ts
@@ -1,21 +1,19 @@
 // 매개변수 이름 다음에 타입 이름 작성
-// function add(n1, n2) {
-function add(n1: number, n2: number) {
-    // console.log(typeof number1);
-
-    // TypeScript 사용을 피하고 있는 코드
-    // if (typeof n1 === 'number' && typeof n2 === 'number') {
-    //     return n1 + n2;
-    // }
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
     // if (typeof n1 !== "number" || typeof n2 !== "number") {
     //     throw new Error("Incorrect input!");
     // }
-    return n1 + n2;
+    const result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
+    } else {
+        return n1 + n2;
+    }
 }
 
 const number1 = 5;
-// const number1 = "5"; // 52.8 출력
 const number2 = 2.8;
+const printResult = true;
+const resultPhrase = "Result is: ";
 
-const result = add(number1, number2);
-console.log(result);
+add(number1, number2, printResult, resultPhrase);
