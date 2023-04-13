2f30e057a034ed3b92ff3e2f0b0af611170d8122
front-chan <chan.jor.zz@gmail.com>
2023-04-13 14:59:23 +0900

    never type

--- start_TS/app.js
+++ start_TS/app.js
@@ -1,3 +1,15 @@
 var userInput;
+// let userInput: any; // 에러 해결
+var userName;
 userInput = 5;
 userInput = "Max";
+// userName = userInput; // 에러 발생
+if (typeof userInput === "string") {
+    userName = userInput; // 에러 발생
+}
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+    // while (true) {} // 무한 루프 생성
+}
+var result = generateError("An error occurred!", 500);
+console.log(result);
--- start_TS/app.ts
+++ start_TS/app.ts
@@ -10,3 +10,11 @@ userInput = "Max";
 if (typeof userInput === "string") {
     userName = userInput; // 에러 발생
 }
+
+function generateError(message: string, code: number): never {
+    throw { message: message, errorCode: code };
+    // while (true) {} // 무한 루프 생성
+}
+
+const result = generateError("An error occurred!", 500);
+console.log(result);
