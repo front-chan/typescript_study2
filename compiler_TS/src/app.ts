75c869172fb443a21ba73dfe68023c0e2704ce94
front-chan <chan.jor.zz@gmail.com>
2023-04-15 02:13:43 +0900

    study checks options

--- compiler_TS/src/app.ts
+++ compiler_TS/src/app.ts
@@ -1,15 +1,31 @@
+let appId = "abc";
+
 const button = document.querySelector("button")!;
+
+function add(n1: number, n2: number) {
+    if (n1 + n2 > 0) {
+        return n1 + n2;
+    }
+    return;
+}
 
 // function clickHandler() {
 //     console.log("Click!!!!");
 // }
 
+// function clickHandler(message: string, age: number) {
+//     // let userName = "Chan";
+//     console.log("Click!!!! " + message);
+// }
+
 function clickHandler(message: string) {
+    // let userName = "Chan";
     console.log("Click!!!! " + message);
 }
 
 if (button) {
     button.addEventListener("click", clickHandler.bind(null, "Hello World"));
+    // button.addEventListener("click", clickHandler.bind(null, "Hello World", 30));
 }
 
 // if (button) {
