72641646a06482b717c093b05eb1bf1f47392948
front-chan <chan.jor.zz@gmail.com>
2023-04-18 00:17:42 +0900

    let and const

--- jsAndts/dist/app.js
+++ jsAndts/dist/app.js
@@ -1,19 +1,15 @@
+"use strict";
 const userName = "Chan";
 // userName = "Tim"
 let age = 15;
-
 age = 29;
-
-function add(n1: number, n2: number) {
+function add(n1, n2) {
     var result;
     result = n1 + n2;
     return result;
 }
-
 // console.log(result);
-
 // if (age > 20) {
 //     let isOld = true;
 // }
-
 // console.log(isOld);
