5497aba18afcc3cd9e199ca786276fbb9765bc72
front-chan <chan.jor.zz@gmail.com>
2023-04-18 02:07:02 +0900

    rest parameter

--- jsAndts/dist/app.js
+++ jsAndts/dist/app.js
@@ -16,16 +16,17 @@ age = 29;
 // const add = (a: number, b: number) => {
 //     return a + b;
 // };
-const add = (a, b = 1) => a + b;
-console.log(add(5));
-console.log(add(2, 5));
-const printOutput = (output) => console.log(output);
-printOutput(add(5, 2));
-const btn = document.querySelector("button");
-if (btn) {
-    btn.addEventListener("click", () => { });
-    btn.addEventListener("click", (event) => console.log(event));
-}
+// const add = (a: number, b: number = 1) => a + b;
+// console.log(add(5));
+// console.log(add(2, 5));
+// const printOutput: (a: string | number) => void = (output: string | number) =>
+//     console.log(output);
+// printOutput(add(5, 2));
+// const btn = document.querySelector("button");
+// if (btn) {
+//     btn.addEventListener("click", () => {});
+//     btn.addEventListener("click", (event) => console.log(event));
+// }
 const hobbies = ["Sports", "Cooking"];
 console.log(hobbies[0]);
 const activeHobbies = ["Hiking"];
@@ -38,3 +39,12 @@ const person = {
 };
 // const copiedPerson = person;
 const copiedPerson = Object.assign({}, person);
+const add = (...numbers) => {
+    // 이렇게 사용할 경우 4번째 인수를 제거해야 한다
+    // const add = (...numbers: [number, number, number]) => {
+    return numbers.reduce((total, curValue) => {
+        return total + curValue;
+    }, 0);
+};
+const addedNum = add(5, 10, 2, 3.7);
+console.log(addedNum);
--- jsAndts/src/app.ts
+++ jsAndts/src/app.ts
@@ -22,23 +22,23 @@ age = 29;
 //     return a + b;
 // };
 
-const add = (a: number, b: number = 1) => a + b;
+// const add = (a: number, b: number = 1) => a + b;
 
-console.log(add(5));
+// console.log(add(5));
 
-console.log(add(2, 5));
+// console.log(add(2, 5));
 
-const printOutput: (a: string | number) => void = (output: string | number) =>
-    console.log(output);
+// const printOutput: (a: string | number) => void = (output: string | number) =>
+//     console.log(output);
 
-printOutput(add(5, 2));
+// printOutput(add(5, 2));
 
-const btn = document.querySelector("button");
+// const btn = document.querySelector("button");
 
-if (btn) {
-    btn.addEventListener("click", () => {});
-    btn.addEventListener("click", (event) => console.log(event));
-}
+// if (btn) {
+//     btn.addEventListener("click", () => {});
+//     btn.addEventListener("click", (event) => console.log(event));
+// }
 
 const hobbies = ["Sports", "Cooking"];
 
@@ -60,3 +60,14 @@ const person = {
 // const copiedPerson = person;
 
 const copiedPerson = { ...person };
+
+const add = (...numbers: number[]) => {
+    // 이렇게 사용할 경우 4번째 인수를 제거해야 한다
+    // const add = (...numbers: [number, number, number]) => {
+    return numbers.reduce((total, curValue) => {
+        return total + curValue;
+    }, 0);
+};
+
+const addedNum = add(5, 10, 2, 3.7);
+console.log(addedNum);
